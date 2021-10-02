8c79ffc5eb71549622acdc55b3e3b3c800a18d3d
kimsan4516 <kimsan4516@naver.com>
2021-10-02 13:54:39 +0900

    UI 구현시작

--- src/main/java/com/portfolio/Flea_Market/HomeController.java
+++ src/main/java/com/portfolio/Flea_Market/HomeController.java
@@ -24,25 +24,14 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		return "login";
+	}
+	@RequestMapping(value = "/home")
+	public String home() {
 		
 		return "home";
 	}
-	
-	
-	@RequestMapping(value = "/login")
-	public String login() {
-		
-		return "login";
-	}	
-	
 	@RequestMapping(value = "/findpassword")
 	public String findpassword() {
 		
